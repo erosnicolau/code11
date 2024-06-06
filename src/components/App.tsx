1a44581d245256b6ea32fa456e286d6431a1edf5
Eros Nicolau <eros.nicolau@gmail.com>
2024-06-06 09:54:08 +0300

    Initial Commit - set up the boilerplate

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="relative overflow-hidden bg-white">
+      <div className="h-screen p-20">
+        <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+          <div>Everything starts here 👋</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
