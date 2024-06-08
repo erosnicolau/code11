c75f499b79e88d5fa39c09d367c9881606d3b3b2
Eros Nicolau <eros.nicolau@gmail.com>
2024-06-08 19:12:08 +0300

    Fixed Bar Leading

--- src/components/Layout/Header/Overview/OverviewItem/index.tsx
+++ src/components/Layout/Header/Overview/OverviewItem/index.tsx
@@ -15,8 +15,8 @@ const OverviewItem: FC<OverviewItemProps> = ({
 }) => {
   return (
     <div className="flex flex-col gap-5 text-nowrap px-2 xl:px-5">
-      <div className="text-[13px] leading-[13px] text-neutral-400">{title}</div>
-      <div className="flex items-center text-sm leading-4 text-neutral-700 xl:text-base">
+      <div className="text-[13px]/[13px] text-neutral-400">{title}</div>
+      <div className="flex items-center text-sm leading-4 text-neutral-700 xl:text-base/4">
         {prefix ? <span className="mr-2 min-w-4">{prefix}</span> : null}
         {description}
         {suffix ? <span className="ml-2 min-w-4">{suffix}</span> : null}
